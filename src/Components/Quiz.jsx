d6f6433875938687dca3f78f0b8305b4bae56413
Akash Sharma <akashsharma91795@gmail.com>
2025-09-24 16:32:22 +0530

    Questions Container Complete

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,16 +1,59 @@
 import './Quiz.css';
-import { Link } from 'react-router';    
-
+import { useNavigate } from 'react-router';    
+import { useState } from 'react';
 function Quiz() {
+  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="quiz-container">
-      <h2>Quiz Component</h2>
-      {/* Quiz content will go here */}
-        <Link to="/results">
-            <button>
-                Finish Quiz
-            </button>
-        </Link>
+      <header>
+        <div className={`hamburger-menu ${isMenuOpen ? 'active' : ''}`}
+        onClick={() => {
+          setIsMenuOpen(!isMenuOpen);
+        }}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        
+        <h1>00:00</h1>
+
+        <button
+        onClick={() => {
+          navigate("/results");
+             }}
+          className='end-quiz-button'> 
+          End
+        </button>
+       
+      </header>
+
+      <nav className={`side-menu ${isMenuOpen ? 'active' : ''}`}>
+
+        <h2>Question Navigator</h2>
+       
+          {[...Array(10)].map((_, index) => (
+            <div key={index} className='question-number'>
+              {index + 1}
+            </div>
+          ))}
+        
+      </nav>
+
+      <button className='previous-button'>
+        &lt;
+      </button>
+
+      <button className='next-button'>
+        &gt;
+      </button>
+      <div className='questions-container'>
+        Questions will be here
+      </div>
+
+      <footer>
+          
+        </footer>
     </div>
   );
 }
